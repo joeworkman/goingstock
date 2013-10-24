ae606151ece0dce60e7b4fc6b893167f52d7d2f0
anovitskiy <anovitskiy+github@gmail.com>
2013-10-24 14:26:04 -0700

    get PORT from env variable in order to support hosting in Modulus

--- API/index.js
+++ API/index.js
@@ -50,4 +50,6 @@ app.delete('/stock/:id', function(req, res) {
 });
 
 //Start the server
-app.listen(8675); //309
+var port = process.env.PORT || 8675;
+app.listen(port);
+console.log('Server running on port ' + port);
